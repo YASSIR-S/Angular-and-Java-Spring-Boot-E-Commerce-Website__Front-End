ed43268f173a784558e821d0c6202019f387b33a
Yassir Salhi <yassir4salhi@gmail.com>
2023-07-31 11:55:03 +0100

    update 3

--- E-Commerce/src/app/components/product-list/product-list.component.ts
+++ E-Commerce/src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,15 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = []; 
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+  thePageNumber: number = 1;
+  thePageSize:number = 5;
+  theTotalElements:number = 0;
+
+  previousKeyword:string = "";
+  
+
   constructor(private productService: ProductService ,
     private route: ActivatedRoute) { }
 
@@ -36,11 +44,17 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts() {
     
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if(this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+    }
+    this.previousKeyword = theKeyword
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+
+    this.productService.searchProductsPaginate(this.thePageNumber -1 , 
+                                              this.thePageSize,
+                                              theKeyword).subscribe(this.processResult());
+      
 
   }
   handleListProducts() {
@@ -51,12 +65,32 @@ export class ProductListComponent implements OnInit {
         this. currentCategoryId=1;
       }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+      if (this.previousCategoryId != this.currentCategoryId){
+        this.thePageNumber = 1;
+       }
+       this.previousCategoryId = this.currentCategoryId;
+       console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber${this.thePageNumber}`);
+
+
+       this.productService.getProductListPaginate(this.thePageNumber -1 , 
+       this.thePageSize,
+       this.currentCategoryId).subscribe(this.processResult());
+
+    
 
   }
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 
+  processResult(){
+    return (data: any) =>{
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number +1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
+}
 }
--- E-Commerce/src/app/services/product.service.ts
+++ E-Commerce/src/app/services/product.service.ts
@@ -14,7 +14,11 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl ='http://localhost:8080/api/product-category';
   constructor(private httpClient: HttpClient) { }
-
+  getProductListPaginate(thePage:number ,thePageSize:number,theCategoryId: number) : Observable<GetResponseProducts>{
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+  return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
   getProductList(theCategoryId: number) : Observable<Product[]>{
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.getProducts(searchUrl);
@@ -23,6 +27,13 @@ export class ProductService {
   searchProducts(theKeyword: string): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
     return this.getProducts(searchUrl);
+  }
+  searchProductsPaginate(thePage:number ,
+                         thePageSize:number,
+                         theKeyword: string) : Observable<GetResponseProducts>{
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+  return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
   
   getProduct(theProductId: number): Observable<Product> {
@@ -45,6 +56,12 @@ export class ProductService {
 interface GetResponseProducts{
   _embedded:{
     products: Product[];
+  },
+  page:{
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
